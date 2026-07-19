91612da5e823330248cab2d966f86f8692498110
WaterToLive <bilalftoure@gmail.com>
2026-07-19 18:38:34 +0000

    checkerboard colors

--- src/App.js
+++ src/App.js
@@ -1,105 +1,125 @@
-function Square() {
-  return <button className="square"></button>;
+import { useState } from 'react';
+
+function Square({color}) {
+const [value, setValue] = useState(null);
+  function handleClick(){
+    setValue('X')
+  }
+  return (
+  <button 
+    className={color}
+    onClick={handleClick}
+  >
+    {value}
+  </button>
+  );
 }
+
 
 export default function Board() {
   return (
     <>
    
       <div className= "board-row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
       
       
       </div>
       
       <div className= "board-row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
       
       
       </div>
-         <div className= "board-row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      
-      
-      </div>
-        <div className= "board-row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      
-      
-      </div>
-         <div className= "board-row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      
-      
-      </div>
-         <div className= "board-row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      
-      
-      </div>
-         <div className= "board-row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      
-      
-      </div>
+         
       <div className= "board-row">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      
+      
+      </div>
+      
+      <div className= "board-row">
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      
+      
+      </div>
+         
+      <div className= "board-row">
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      
+      
+      </div>
+      
+      <div className= "board-row">
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      
+      
+      </div>
+         
+      <div className= "board-row">
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      
+      
+      </div>
+      
+      <div className= "board-row">
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
+      <Square  color = "squareB"/>
+      <Square  color = "square"/>
       
       
       </div>
